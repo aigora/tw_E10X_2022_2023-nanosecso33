8d8f9dd8c67e1ae4d4729f4dc76215c54872825f
oonetoo <j.oneto@alumnos.upm.es>
2023-05-24 00:44:26 +0200

    actualizacion

--- trabajo.c
+++ trabajo.c
@@ -5,7 +5,7 @@ int main()
 {
 	printf("Acaba de ingresar en la pagina de datos de 2021 y 2022 de la Red Electrica Espanola.\n");
 	printf("Si desea cargar el fichero para analizar y consultar los datos pulse 1, si no desea acceder a los datos pulse 2.\n");
-	int opcion;
+	int opcion, opcion2;
 	scanf("%i", &opcion);
 	switch(opcion)
 	{
@@ -16,13 +16,38 @@ int main()
 			printf("Ha seleccionado no cargar el fichero, programa finalizado.\n");
 			break;
 		default:
-			printf("Algo salio mal, programa interrumpido.");
+			printf("Algo salio mal, programa interrumpido.\n");
 			break;			
+	}
+	
+	if(opcion!=1)
+	{
+		printf("Si desea intentarlo de nuevo pulsa 1.\n");
+		scanf("%i", &opcion2);
+		if(opcion2==1)
+		{
+			main();
+		}
+		else
+		{
+			printf("Programa finalizado definitivamente.");
+		}
 	}
 	
 	if(opcion==1)
 	{
-		
+		FILE *pf;
+		pf = fopen("C:/Users/VORPC/Documents/GitHub/tw_E10X_2022_2023-nanosecso33/generacion_por_tecnologias_21_22_puntos_simplificado.csv", "r");
+		if (pf == NULL)
+		{
+			printf("Error al abrir el fichero.\n");
+			return -1;
+		}
+		else
+		{
+			printf("Fichero abierto correctamente.\n");
+			return 0;
+		}
 	}
 	
 	
